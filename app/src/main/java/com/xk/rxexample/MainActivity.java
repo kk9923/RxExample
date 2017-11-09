27fec1753d236ed07626f99ed308b98a64fa9f8c
renwoxing <renwoxing@yuruiqi.com>
2017-11-09 17:48:05 +0800

    学习RxJava2.x 中新对象 ObservableEmitter。 Disposable的用法

--- app/src/main/java/com/xk/rxexample/MainActivity.java
+++ app/src/main/java/com/xk/rxexample/MainActivity.java
@@ -1,13 +1,63 @@
 package com.xk.rxexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                Log.e(TAG, "emit 1");
+                emitter.onNext(1);
+                Log.e(TAG, "emit 2");
+                emitter.onNext(2);
+                Log.e(TAG, "emit 3");
+                emitter.onNext(3);
+                Log.e(TAG, "emit complete");
+                emitter.onComplete();
+                Log.e(TAG, "emit 4");
+                emitter.onNext(4);
+            }
+        }).subscribe();
+//                .subscribe(new Observer<Integer>() {
+//            private Disposable mDisposable;
+//            private int i;
+//
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                Log.e(TAG, "subscribe");
+//                mDisposable = d;
+//            }
+//
+//            @Override
+//            public void onNext(Integer value) {
+//                Log.e(TAG, "onNext: " + value);
+//                i++;
+//                if (i == 2) {
+//                    Log.e(TAG, "dispose");
+//                    mDisposable.dispose();
+//                    Log.e(TAG, "isDisposed : " + mDisposable.isDisposed());
+//                }
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.e(TAG, "error");
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                Log.d(TAG, "complete");
+//            }
+//        });
+}}
